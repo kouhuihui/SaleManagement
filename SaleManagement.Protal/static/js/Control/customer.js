a3bb96a7c8bbf4470cf59ec560874268dd225c97
khh <344076569@qq.com>
2016-10-19 23:09:02 +0800

    修改js报错

--- SaleManagement.Protal/static/js/Control/customer.js
+++ SaleManagement.Protal/static/js/Control/customer.js
@@ -1,93 +1,98 @@
 ﻿var defaults = {
-	width: '150px',
-	callback: function (data) {
-		return data;
-	}
+    width: '150px',
+    callback: function (data) {
+        return data;
+    }
 }
-
-var Customer = function (options) {
-	this.options = $.extend({}, defaults, options);
-	this.init();
-};
-var $customerCombobox = $("#customerCombobox");
-var $vCustomerInput = $customerCombobox.find("#vCustomerId");
-var $customerId = $customerCombobox.find("#customerId");
-var $customerUl = $customerCombobox.find("ul");
+var $customerCombobox;
+var $vCustomerInput;
+var $customerId;
+var $customerUl;
 var customerstore = [];
 var originCustomerStore = [];
+var Customer = function (options) {
+    $customerCombobox = $("#customerCombobox");
+    $vCustomerInput = $customerCombobox.find("#vCustomerId");
+    $customerId = $customerCombobox.find("#customerId");
+    $customerUl = $customerCombobox.find("ul");
+    customerstore = [];
+    originCustomerStore = [];
+    this.options = $.extend({}, defaults, options);
+    this.init();
+};
 
 Customer.prototype = {
-	init: function () {
-		this.getStore();
-		this.method();
-	},
-	getStore: function () {
-		var _this = this;
-		$.ajax({
-			url: "/crm/customers",
-			success: function (rtn) {
-				if (rtn.succeeded) {
-					var data = rtn.data;
-					customerstore = data;
-					_this.initSelect();
-					_this.getOriginStore();
-				}
-			},
-			error: function () {
+    init: function () {
+        this.getStore();
+        this.method();
+    },
+    getStore: function () {
+        var _this = this;
+        $.ajax({
+            url: "/crm/customers",
+            success: function (rtn) {
+                if (rtn.succeeded) {
+                    var data = rtn.data;
+                    customerstore = data;
+                    _this.initSelect();
+                    _this.getOriginStore();
+                }
+            },
+            error: function () {
 
-			}
-		});
-	},
-	initSelect: function () {
-		var html = '<li value="">所有客户</li>';
-		for (var i = 0, len = customerstore.length; i < len; i++) {
-			html += '<li value="' + customerstore[i].value
+            }
+        });
+    },
+    initSelect: function () {
+        var html = '<li value="">所有客户</li>';
+        for (var i = 0, len = customerstore.length; i < len; i++) {
+            html += '<li value="' + customerstore[i].value
 				+ '">' + customerstore[i].text
 				+ '</li>';
-		}
-		$customerUl.html(html);
-	},
-	getOriginStore: function () {
-		originCustomerStore = customerstore.concat([]);
-	},
-	doSearch: function () {
-		var arr = [];
-		for (var i = 0, items = originCustomerStore, len = items.length; i < len; i++) {
-			if (items[i].text.toLowerCase().trim().indexOf($vCustomerInput.val().toLowerCase().trim()) !== -1) {
-				arr.push(items[i]);
-			}
-		}
-		customerstore = arr;
-		this.initSelect();
-	},
-	getValue:function(){
-	    return $customerId.val();
-	},
-	getText:function(){
-	    return $vCustomerInput.val();
-	},
-	method: function () {
-		var _this = this;
-		$vCustomerInput.bind("click", function () {
-			$vCustomerInput.focus();
-			$vCustomerInput.select();
-			$customerUl.show();
-		});
-		$vCustomerInput.on("keyup", function () {
-			_this.doSearch();
-		});
-		$customerUl.delegate("li", "click", function () {
-			var $this = $(this);
-			var value = $this.attr("value");
-			if (value === "") {
-				$vCustomerInput.val("");
-				$customerId.val("");
-			} else {
-				$vCustomerInput.val($(this).text());
-				$customerId.val(value);
-			}
-			$customerUl.hide();
-			_this.options.callback(value);
-		});
-	}
+        }
+        $customerUl.html(html);
+    },
+    getOriginStore: function () {
+        originCustomerStore = customerstore.concat([]);
+    },
+    doSearch: function () {
+        var arr = [];
+        for (var i = 0, items = originCustomerStore, len = items.length; i < len; i++) {
+            if (items[i].text.toLowerCase().trim().indexOf($vCustomerInput.val().toLowerCase().trim()) !== -1) {
+                arr.push(items[i]);
+            }
+        }
+        customerstore = arr;
+        this.initSelect();
+    },
+    getValue: function () {
+        return $customerId.val();
+    },
+    getText: function () {
+        return $vCustomerInput.val();
+    },
+    method: function () {
+        var _this = this;
+        $vCustomerInput.bind("click", function () {
+            $vCustomerInput.focus();
+            $vCustomerInput.select();
+            $customerUl.show();
+        });
+        $vCustomerInput.on("keyup", function () {
+            _this.doSearch();
+        });
+        $customerUl.delegate("li", "click", function () {
+            var $this = $(this);
+            var value = $this.attr("value");
+            if (value === "") {
+                $vCustomerInput.val("");
+                $customerId.val("");
+            } else {
+                $vCustomerInput.val($(this).text());
+                $customerId.val(value);
+            }
+            $customerUl.hide();
+            _this.options.callback(value);
+        });
+    }
 };
